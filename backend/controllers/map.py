f1425670625467e4fb843cb6e8598d8d88c9c020
Denis Filin <formok8@gmail.com>
2024-10-22 02:12:22 +0300

    add redis streams

--- backend/controllers/map.py
+++ backend/controllers/map.py
@@ -5,24 +5,38 @@ from model.core import ModelInterface,User,Battle
 
 async def get_mapWS(websocket:WebSocket):
     try:
-        await manager.preconnect(websocket)
+        await manager.connect(websocket)
         data = websocket.cookies.get("Authorization")
         id_info = get_access_token(data)
         if not await ModelInterface.get_model_filter(model=User,filter=id_info.get("email")):
-            await manager.predisconnect(websocket)
+            await manager.disconnect(websocket)        
     except:
-        await manager.predisconnect(websocket)
-    else:
-        await manager.connect(websocket)
-        while True:
-            data = await websocket.receive_json()
-            if isinstance(data.get("pixel"),str) and isinstance(data.get("color"),str):
-                if not await ModelInterface.get_model_filter(model=Battle,filter=data.get("pixel")):
-                    await ModelInterface.set_battle_pixel_color(pixel=data.get("pixel"),color=data.get("color"))
-                else:
-                    await ModelInterface.update_model(model=Battle, _id=data.get("pixel"), data={"color": data['color']})
-                await manager.broadcast({"pixel":data.get("pixel"),"color":data.get("color")})
+        await manager.disconnect(websocket)  
+    try:      
+        await ModelInterface.get_stream(websocket)
+    except:
+        await manager.disconnect(websocket)
 
+async def set_mapWS(websocket:WebSocket):
+    try:
+        await manager.connect(websocket)
+        data = websocket.cookies.get("Authorization")
+        id_info = get_access_token(data)
+    except:
+        await manager.disconnect(websocket)  
+    try:      
+        if not await ModelInterface.get_model_filter(model=User,filter=id_info.get("email")):
+            await manager.disconnect(websocket)
+
+        else:
+            while True:
+                data = await websocket.receive_json()
+                if isinstance(data.get("pixel"),str) and isinstance(data.get("color"),str):
+                    await ModelInterface.set_stream_pixelbattle(pixel=data.get("pixel"),color=data.get("color"))
+    except:
+        await manager.disconnect(websocket)
+
+    
 async def get_map(Authorization):
     id_info = get_access_token(Authorization)
     if not await ModelInterface.get_model_filter(model=User,filter=id_info.get("email")):
